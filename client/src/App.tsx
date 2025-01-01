97e6a1f02dda969da5c2c4ae034bb538a5dc493c
kayoMichael <michaelxkayo@hotmail.com>
2025-01-01 11:33:18 -0500

    Finish up Navbar and implement scripts in root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Navbar />} />
-      <Route path="/recent" element={<div>Recent</div>} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={<h2></h2>} />
+        <Route path="/recent" element={<div>Recent</div>} />
+      </Routes>
+    </>
   );
 }
 

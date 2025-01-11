2c315e73d9f93545ad1bb8572c8cebbfcca0d59f
kayoMichael <michaelxkayo@hotmail.com>
2025-01-11 15:04:10 -0500

    configure cors to allow for preview deployment url

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,8 +1,30 @@
 import { env } from './env.js';
 
-const corsConfig = {
+interface CorsConfig {
+  credentials: boolean;
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => void;
+  methods: string[];
+  allowedHeaders: string[];
+}
+
+const corsConfig: CorsConfig = {
   credentials: true,
-  origin: env.CLIENT_HOST,
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (env.CLIENT_HOST === 'run.app') {
+      const pattern = new RegExp('.*\\.run\\.app$');
+      if (!origin || pattern.test(origin)) {
+        callback(null, true);
+        return;
+      }
+    } else if (!origin || origin === env.CLIENT_HOST) {
+      callback(null, true);
+      return;
+    }
+    callback(new Error('Not allowed by CORS'));
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };

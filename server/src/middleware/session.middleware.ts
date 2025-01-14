60c3d1bca8e67031afd27a0ac586bb6f1e47786d
kayoMichael <michaelxkayo@hotmail.com>
2025-01-14 14:37:07 -0500

    delete domain from cookie due to public suffix list problem

--- server/src/middleware/session.middleware.ts
+++ server/src/middleware/session.middleware.ts
@@ -6,19 +6,19 @@ import { env } from '../config/env.js';
 import redisClient from '../config/redis.js';
 
 const sessionMiddleware: RequestHandler = session({
-  name: 'session',
+  name: '__session',
   store: new RedisStore({
     client: redisClient,
   }),
   secret: env.SESSION_SECRET_KEY,
   resave: false,
   saveUninitialized: false,
+  proxy: true,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    ...(process.env.NODE_ENV === 'production' ? { domain: '.run.app' } : {}),
   },
 });
 
